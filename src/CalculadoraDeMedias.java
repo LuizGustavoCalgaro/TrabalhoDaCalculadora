8ba9b5fe9a8475e794fb414717a9996c9d0af9ff
otaviobleich <ferreiraotavio577@gmail.com>
2024-11-19 20:10:42 -0300

    tentando criar uma calculadora

---
+++ src/CalculadoraDeMedias.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class CalculadoraDeMedias {
+    private JPanel AplicativodeNotas;
+    private JTextField BtnNotas;
+    private JButton BtnAddNota;
+    private JTextArea AreaDeExibicao;
+    private JButton BtnCalculo;
+}
